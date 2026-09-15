1c256b72f8b2d2028f5c2ab89b613bb3b87d4a80
Karthikeya Abhiram <youremail@example.com>
2026-09-15 20:00:09 +0530

    Fix division by zero error

--- calc.sh
+++ calc.sh
@@ -5,3 +5,8 @@ read -p "Enter second number: " b
 echo "Sum = $((a + b))"
 echo "Difference = $((a - b))" 
 echo "Product = $((a * b))"
+if [ $b -eq 0 ]; then
+    echo "Cannot divide by zero"
+else
+    echo "Quotient = $((a / b))"
+fi
